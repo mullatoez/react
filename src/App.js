c29eac2b56b5b870f006c53ced3345b8d776bc60
mullatoez <kimanthikevin76@gmail.com>
2022-11-26 17:08:53 +0300

    useState walkthrough

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import './App.css';
 import './components/Employee'
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
 
   const showEmployees = true;
+  const [role, setRole] = useState();
 
   return (
     <div className="App">
       {
         showEmployees ? 
         <>
+        <input 
+        type="text" 
+        onChange={(el) => {
+          console.log(el.target.value)
+          setRole(el.target.value);
+        }}
+         />
          <Employee name="Kevin" role="Cross platform app developer"/>
-         <Employee name="Naomi"/>
+         <Employee name="Naomi" role={role}/>
          <Employee name="Annie"/>
         </>
         :
